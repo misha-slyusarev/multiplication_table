6817d12e2527122b9517c6e851204777466b7c5f
Misha Slyusarev <mik3weider@gmail.com>
2017-05-04 20:43:06 +0200

    Add PrimeGenerator

--- lib/multiplication_table/prime_generator.rb
+++ lib/multiplication_table/prime_generator.rb
@@ -1,2 +1,19 @@
 class MultiplicationTable::PrimeGenerator
+  def initialize(first = 1)
+    @current = first
+  end
+
+  def next
+    @current += 1
+    until prime?(@current) do
+      @current += 1
+    end
+    @current
+  end
+
+  private
+
+    def prime?(n)
+      (2..n/2).none?{|i| n % i == 0}
+    end
 end
