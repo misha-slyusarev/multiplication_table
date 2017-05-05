cd5d1bdee375492bbddf6ab923ff35f7988a96fc
Misha Slyusarev <mik3weider@gmail.com>
2017-05-05 17:20:52 +0200

    Implement Table class

--- lib/multiplication_table/table.rb
+++ lib/multiplication_table/table.rb
@@ -1,2 +1,29 @@
 class MultiplicationTable::Table
+  def initialize(generator)
+    @generator = generator
+  end
+
+  def print_out(length)
+    if length <= 0
+      puts 'Sequence length must be more than zero'
+      return
+    end
+
+    sequence = @generator.get(length)
+    @column_width = Math.log10(length**2).ceil + 1
+
+    puts sequence.reduce(' ' *  @column_width) {|l, x| l << ' ' << adjust(x)}
+
+    sequence.each do |x|
+      puts sequence.reduce(adjust(x)) {|l, y| l << ' ' << adjust(x * y)}
+    end
+
+    return
+  end
+
+  private
+
+    def adjust(n)
+      n.to_s.rjust(@column_width)
+    end
 end
