1fa2101087cd08ad540379305a787a0717c076e1
Misha Slyusarev <mik3weider@gmail.com>
2017-05-05 17:19:17 +0200

    Update table tests
    We move length parameter to ‘print_out’ method, so you can generate
    tables of different sizes. Also the table doesn’t take a class of
    generator anymore, now it works with just an instance.

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,26 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe MultiplicationTable::Table do
-  subject { MultiplicationTable::Table.new(seq_generator_class, seq_length) }
+  subject { MultiplicationTable::Table.new(seq_generator) }
 
   describe '#print_out' do
     context 'with PrimeGenerator as sequence generator' do
-      let(:seq_generator_class) { MultiplicationTable::PrimeGenerator }
+      let(:seq_generator) { MultiplicationTable::PrimeGenerator.new }
 
       context 'with sequence length more then 0' do
         let(:seq_length) { 2 }
-        let(:valid_table) { '  2 3\n2 4 6\n3 6 9' }
+        let(:valid_table) { "    2  3\n 2  4  6\n 3  6  9\n" }
 
         it 'prints out prime multiplication table' do
-          expect(subject.print_out).to eq(valid_table)
+          expect{subject.print_out(seq_length)}.to output(valid_table).to_stdout
         end
       end
 
       context 'with sequence length less then 1' do
-        let(:seq_length) { -1 }
-
         it 'prints out error message' do
-          expect(subject.print_out).to eq("Don't know what to print")
+          expect{subject.print_out(-1)}.to output("Sequence length must be more than zero\n").to_stdout
         end
       end
     end
