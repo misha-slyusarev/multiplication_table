d940373b74c45e6ceec22232010462cb696ac6f1
Misha Slyusarev <mik3weider@gmail.com>
2017-05-05 19:45:48 +0200

    Generalise 'less 1 sequence' specification
    Table prints out error message regardless generator. So it makes sense
    to move this specification out of PrimeGenerator context to the general
    level of Table behaviour.

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -6,19 +6,17 @@ RSpec.describe MultiplicationTable::Table do
 
   describe '#print_out' do
     context 'with PrimeGenerator as sequence generator' do
-      context 'with sequence length more then 0' do
-        let(:seq_length) { 2 }
-        let(:valid_table) { "  2 3\n2 4 6\n3 6 9\n" }
+      let(:seq_length) { 2 }
+      let(:valid_table) { "  2 3\n2 4 6\n3 6 9\n" }
 
-        it 'prints out prime multiplication table' do
-          expect{subject.print_out(seq_length)}.to output(valid_table).to_stdout
-        end
+      it 'prints out prime multiplication table' do
+        expect{subject.print_out(seq_length)}.to output(valid_table).to_stdout
       end
+    end
 
-      context 'with sequence length less then 1' do
-        it 'prints out error message' do
-          expect{subject.print_out(-1)}.to output("Sequence length must be more than zero\n").to_stdout
-        end
+    context 'with sequence length less then 1' do
+      it 'prints out error message' do
+        expect{subject.print_out(-1)}.to output("Sequence length must be more than zero\n").to_stdout
       end
     end
 
