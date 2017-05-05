d049fe05c518473ffd001f1d69d078ee502d9d4d
Misha Slyusarev <mik3weider@gmail.com>
2017-05-06 01:22:01 +0200

    Use max value of a sequence to determine column width

--- lib/multiplication_table/table.rb
+++ lib/multiplication_table/table.rb
@@ -5,17 +5,16 @@ class MultiplicationTable::Table
 
   def print_out(length)
     if length <= 0
-      puts 'Sequence length must be more than zero'
+      puts('Sequence length must be more than zero')
       return
     end
 
     sequence = @generator.get(length)
-    @column_width = Math.log10(sequence.last**2).ceil
+    @column_width = Math.log10(sequence.max**2).ceil
 
-    puts sequence.reduce(' ' *  @column_width) {|l, x| l << ' ' << adjust(x)}
-
+    puts(sequence.reduce(' ' *  @column_width) {|l, x| l << ' ' << adjust(x)})
     sequence.each do |x|
-      puts sequence.reduce(adjust(x)) {|l, y| l << ' ' << adjust(x * y)}
+      puts(sequence.reduce(adjust(x)) {|l, y| l << ' ' << adjust(x * y)})
     end
 
     return
