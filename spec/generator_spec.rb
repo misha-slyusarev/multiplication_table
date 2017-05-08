c9e569aa693baef6995f396f9cc9b73be300d606
Misha Slyusarev <mik3weider@gmail.com>
2017-05-08 02:19:30 +0200

    Check that a generator returns array of numeric

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -2,8 +2,9 @@ shared_examples_for 'a generator' do
   it { is_expected.to respond_to(:get).with(1).argument }
 
   describe '#get' do
-    it 'returns Array' do
-      expect(subject.get(2)).to be_a_kind_of(Array)
+
+    it 'returns Array of Numeric' do
+      expect(subject.get(3)).to all(be_a(Numeric))
     end
   end
 end
